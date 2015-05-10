13c9d755cd2d2453ebe5a34421b0f6af1515cbcc
Quentin Pré <pre.quentin@gmail.com>
2015-05-10 16:17:34 +0200

    [ADD] Get Shit Done Bootstrap UI Kit

--- Brocfile.js
+++ Brocfile.js
@@ -5,12 +5,21 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-
-
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-
-
 app.import('bower_components/fontawesome/css/font-awesome.min.css');
+
+
+app.import('vendor/GSDK/assets/css/demo.css');
+app.import('vendor/GSDK/assets/css/get-shit-done.css');
+app.import('vendor/GSDK/assets/css/gsdk-base.css');
+app.import('vendor/GSDK/assets/css/gsdk-checkbox-radio-switch.css');
+app.import('vendor/GSDK/assets/css/gsdk-sliders.css');
+
+app.import('vendor/GSDK/assets/js/custom.js');
+app.import('vendor/GSDK/assets/js/get-shit-done.js');
+app.import('vendor/GSDK/assets/js/gsdk-bootstrapswitch.js');
+app.import('vendor/GSDK/assets/js/gsdk-checkbox.js');
+app.import('vendor/GSDK/assets/js/gsdk-radio.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
