cfaebc9e8b98556a9c53c965fcef5afac07be50d
Quentin Pré <pre.quentin@gmail.com>
2015-05-10 14:08:14 +0200

    tst

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ DEFAULT="jenkins"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=phubo
 DIR=dist/
-aws  s3  sync $DIR s3://$BUCKET/ --profile "$PROFILE"
+aws s3 sync $DIR s3://$BUCKET/ --profile "$PROFILE"
