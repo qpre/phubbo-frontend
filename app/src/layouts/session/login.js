eb15da8470bacf61f6dac6d54156bde502fdcc1c
Quentin Pre <pre.quentin@gmail.com>
2015-08-19 09:53:56 +0200

    [ADD] basic user reckognition

--- app/src/layouts/session/login.js
+++ app/src/layouts/session/login.js
@@ -9,15 +9,20 @@ export default class SessionLoginLayout extends React.Component {
     this.state = {
       username: '',
       password: '',
-      knownUsers: [] 
+      knownUsers: []
     }
+  }
+
+  componentWillMount () {
+    let users = Store.get('knownUsers');
+    users && this.setState({knownUsers: Store.get('knownUsers') });
   }
 
   validate () {
     if ((this.state.username === 'test') && (this.state.password === 'toto')) {
       Store.set('name', this.state.username);
-      this.state.knownUsers.push({name: this.state.username});
-      Store.set('knowUsers', this.state.knownUsers);
+      this.state.knownUsers.push({ name: this.state.username });
+      Store.set('knownUsers', this.state.knownUsers);
       navigate('/');
     } else {
       (document.getElementsByClassName('session-login-layout'))[0].classList.add('shake');
@@ -30,12 +35,23 @@ export default class SessionLoginLayout extends React.Component {
     this.setState(state);
   }
 
+  selectUser (user) {
+    return () => {
+      this.setState({ username: user.name });
+    }
+  }
+
   renderKnownUsers () {
     return this.state.knownUsers.map((user) => {
-      return <li>{user.name}</li>
+      return <li onClick={this.selectUser(user)}>
+        <div className='icon'>
+          <img src={user.icon} />
+        </div>
+        <div className='name'>{user.name}</div>
+      </li>;
     })
   }
-    
+
   render () {
     return <div className='session-login-layout centered-content fadeIn animated'>
       <ul className='usericons'>
