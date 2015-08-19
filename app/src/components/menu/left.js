ded1059c418ba3bbf125ce713cde5c000c3eaab7
Quentin Pre <pre.quentin@gmail.com>
2015-08-19 07:43:16 +0200

    [FIX] login view

--- app/src/components/menu/left.js
+++ app/src/components/menu/left.js
@@ -1,5 +1,6 @@
 import {publish, subscribe} from '../../modules/Notifier/notifier';
 import {checkAuthorized} from '../../modules/Router/filters';
+import {logout} from '../../modules/Router/filters';
 import * as Store from '../../modules/Data/Store';
 
 export class MenuLeft extends React.Component {
@@ -17,12 +18,17 @@ export class MenuLeft extends React.Component {
     this.setState({ name: Store.get('name') });
   }
 
+  logOut () {
+    Store.set('name', undefined);
+    checkAuthorized();
+  }
+
   render () {
     return <ul className='menu left'>
       <li className='profile'>
         <div className='cell photo'><img src='https://scontent-ams2-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/537913_10153030282134869_859092913535597309_n.jpg?oh=1a6b7640becae3229e17fadd96919d9c&oe=56395BE7' /></div>
         <div className='cell name'>{this.state.name}</div>
-        <div className='cell logout'><i className='pe-7s-close'></i></div>
+        <div className='cell logout'><i className='pe-7s-close' onClick={this.logOut}></i></div>
       </li>
       <li>photos</li>
     </ul>;
