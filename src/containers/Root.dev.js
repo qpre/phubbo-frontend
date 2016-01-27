0162550add82cfb6fc99b59ac7c5049801cbf039
Quentin Pre <pre.quentin@gmail.com>
2016-01-28 00:29:28 +0100

    [RM] devtools

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -8,10 +8,7 @@ export default class Root extends Component {
     const { store } = this.props;
     return (
       <Provider store={store}>
-        <div>
-          <Application />
-          <DevTools />
-        </div>
+        <Application />
       </Provider>
     );
   }
